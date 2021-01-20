a21da37e9c68e455034e74b6c068b798164ec776
Marc Steele <marcsteele2014@gmail.com>
2021-01-20 20:51:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,8 +4,8 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
-  self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym
+  self.column_names.each do |col|
+    attr_accessor col.to_sym
   end
 
 end
